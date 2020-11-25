d00982a00d33459e649bfe6b079f8d0094d03b90
Lorenzoctt <lorenzo.ciatti64@gmail.com>
2020-11-25 15:39:04 +0100

    Modifiche per esercizi in aula

--- Program.cs
+++ Program.cs
@@ -6,13 +6,32 @@ namespace CSharpExercises
     {
         static void Main(string[] args)
         {
+            {  // mio esempio
+                string aFriend = "Bill";
+                Console.WriteLine($"Hello {aFriend}");
+                Console.Write("Premi un tasto per proseguire");
+                Console.ReadKey();
 
-            string aFriend = "Bill";
-            DateTime nowDate = DateTime.Today; 
-            Console.WriteLine($"Hello {aFriend}");
-            Console.WriteLine($"Oggi è il  {nowDate}");
+                string nowDate = DateTime.Today.Date.ToString(("dd/MM/yyyy")) , nowHour = DateTime.Now.ToString("HH:mm:ss");
+                Console.WriteLine($"Oggi è il  {nowDate} e sono le ore {nowHour}");
+            }
+            {
 
+                //   Scrivi un programma completo di namespace, che legge il tuo nome dalla console e lo ristampa 
+                //    trasformandolo in uppercase.Carica le modifiche sul repo Github.
+                Console.WriteLine("Immetti il tuo nome");
+                string retName = Console.ReadLine().ToUpper();
+                Console.WriteLine($"Ciao {retName}");
 
+            }
+            {
+                // Scrivi un programma completo di namespace, che legge il tuo nome dagli argomenti di Main() e lo 
+                // ristampa trasformandolo in uppercase.Carica le modifiche sul repo Github.
+                if (args.Length > 0) {
+                    Console.WriteLine($"Il parametro passato è {args[0]}");
+                        }
+                else Console.WriteLine("Nessun parametro passato !");
+            }
 
 
 
