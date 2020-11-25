7db07b5fad4bfbf73dfe5de89b8946c65d1d04b5
Lorenzoctt <lorenzo.ciatti64@gmail.com>
2020-11-25 10:19:22 +0100

    ID=xxxx fatta variazione con passaggio variabile

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace CSharpExercises
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
+            
 
         }
     }
