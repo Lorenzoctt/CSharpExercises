4e9440876a14d2efd3dc3ed798d1f36368aa91b8
Lorenzoctt <lorenzo.ciatti64@gmail.com>
2020-12-01 18:56:55 +0100

    ok

--- Program.cs
+++ Program.cs
@@ -13,14 +13,12 @@ class Program
     {
         static void Main(string[] args)
         {
-            TestClassDerivata();
+         
             {  // mio esempio
                 string aFriend = "Bill";
                 Console.WriteLine($"Hello {aFriend}");
                 Console.Write("Premi un tasto per proseguire");
                 Console.ReadKey();
-
-
 
 
                 string nowDate = DateTime.Today.Date.ToString(("dd/MM/yyyy")), nowHour = DateTime.Now.ToString("HH:mm:ss");
@@ -111,37 +109,7 @@ class Program
 
 
 
-        static  void TestClassDerivata()
-        {
-           
-            // Create an instance of WorkItem by using the constructor in the
-            // base class that takes three arguments.
-            WorkItem item = new WorkItem("Fix Bugs",
-                                        "Fix all bugs in my code branch",
-                                        new TimeSpan(3, 4, 0, 0));
-
-        // Create an instance of ChangeRequest by using the constructor in
-        // the derived class that takes four arguments.
-        ChangeRequest change = new ChangeRequest("Change Base Class Design",
-                                                "Add members to the class",
-                                                new TimeSpan(4, 0, 0),
-                                                1);
-
-        // Use the ToString method defined in WorkItem.
-        Console.WriteLine(item.ToString());
-
-            // Use the inherited Update method to change the title of the
-            // ChangeRequest object.
-            change.Update("Change the Design of the Base Class",
-                new TimeSpan(4, 0, 0));
-
-            // ChangeRequest inherits WorkItem's override of ToString.
-            Console.WriteLine(change.ToString());
-            /* Output:
-                1 - Fix Bugs
-                2 - Change the Design of the Base Class
-            */
-        }
+       
 
 }
 
